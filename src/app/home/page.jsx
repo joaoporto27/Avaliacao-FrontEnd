ee8eca22ce88139a46f77143eb117fb2e40dd66b
joaoporto27 <joao.v.sales15@aluno.senai.br>
2025-05-22 09:55:29 -0300

    Feat: salvação

---
+++ src/app/home/page.jsx
@@ -0,0 +1,36 @@
+"use client";
+
+import styles from "./Home.module.css";
+import { Card, Button } from "antd";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+    return (
+        <div className={styles.container}>
+            <Card hoverable className={styles.card}>
+                <div className={styles.imageProfile}>
+                    <Image
+                        src="/images/profile.jpeg"
+                        alt="example"
+                        width={300}
+                        height={200}
+                        className={styles.image}
+                    />
+                </div>
+                <div className={styles.description}>
+                    <h2 >João Vitor Porto Sales</h2>
+                    <h3>2TDS1</h3>
+                    <h3>Esta API tem o propósito de mostrar a implementação do front-end com back-end, para isso irei utilizar as tabelas Usuários e Posts.</h3>
+                </div>
+                <div className={styles.button}>
+                    <Link href="/usuarios">
+                        <Button type="dashed" className={styles.colorButton}>Ver API de Usuarios e Posts</Button>
+                    </Link>
+                </div>
+                    <h5> Docentes: Thiago Ferreira e Marcelo Carboni </h5>
+            </Card>
+
+        </div>
+    );
+}
---
+++ src/app/usuarios/page.jsx
@@ -0,0 +1,10 @@
+import styles from "./Usuarios.module.css";
+
+export default function Usuarios() {
+    return (
+    <div>
+        <h1 className={styles.title}>AAAAAAAA</h1>
+    </div>
+    );
+}
+
